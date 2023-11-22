73af637e27d6751930c6909132868b382da57a1f
nikayel <alinikayeljamal@gmail.com>
2023-11-22 14:42:09 -0800

    fix .h file error by Maryam

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -1,55 +1,48 @@
+#pragma once
+#ifndef COMPLEXPLANE_H
+#define COMPLEXPLANE_H
+
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp> // Include the audio library (not implemented yet)
-#include <cmath>
-#include <vector>
-#include <iostream>
-#include <sstream>
+using namespace sf;
 
-using namespace std; // Use the std namespace
+const unsigned int MAX_ITER = 64;
+const float BASE_WIDTH = 4.0;
+const float BASE_HEIGHT = 4.0;
+const float BASE_ZOOM = 0.5;
 
-class ComplexPlane : public sf::Drawable
+enum class State
+{
+    CALCULATING,
+    DISPLAYING
+};
+
+class ComplexPlane : public Drawable
 {
 public:
-    // Constants
-    const unsigned int MAX_ITER = 64;
-    const float BASE_WIDTH = 4.0;
-    const float BASE_HEIGHT = 4.0;
-    const float BASE_ZOOM = 0.5;
-
-    // Enum class
-    enum class State
-    {
-        CALCULATING,
-        DISPLAYING
-    };
-
-    // Constructors and destructor
     ComplexPlane(int pixelWidth, int pixelHeight);
-
-    // Member functions
-    void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
+    void draw(RenderTarget &target, RenderStates states) const;
     void updateRender();
     void zoomIn();
     void zoomOut();
-    void setCenter(sf::Vector2i mousePixel);
-    void setMouseLocation(sf::Vector2i mousePixel);
-    void loadText(sf::Text &text);
-    size_t countIterations(sf::Vector2f coord);
-    void iterationsToRGB(size_t count, sf::Uint8 &r, sf::Uint8 &g, sf::Uint8 &b);
-    sf::Vector2f mapPixelToCoords(sf::Vector2i mousePixel);
+    void setCenter(Vector2i mousePixel);
+    void setMouseLocation(Vector2i mousPixel);
+    void loadText(Text &text);
 
 private:
-    // Private member functions
-    void initializeVertexArray();
-
-    // Private member variables
+    float m_aspectRatio;
+    int m_ZoomCount;
+    Vector2f m_plane_size;
+    Vector2f m_plane_center;
+    Vector2i m_pixel_size;
+    Vector2f m_mouseLocation;
+    State m_state;
+    VertexArray m_vArray;
     int m_pixelWidth;
     int m_pixelHeight;
-    float m_aspectRatio;
-    sf::Vector2f m_plane_center;
-    sf::Vector2f m_plane_size;
-    int m_zoomCount;
-    State m_State;
-    sf::VertexArray m_vArray;
-    sf::Vector2f m_mouseLocation;
+
+    int countIterations(Vector2f coord);
+    void iterationsToRGB(size_t count, Uint8 &r, Uint8 &g, Uint8 &b);
+    Vector2f mapPixelToCoords(Vector2i mousePixel);
 };
+
+#endif
